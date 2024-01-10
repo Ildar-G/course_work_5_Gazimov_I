d80d83bf3023b5f8cca2cbac89b925d55c7e6d8e
Ildar-G <igazimov83@gmail.com>
2024-01-11 00:00:55 +0500

    update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from confing import config
 from DBManager import DBManager
 
 params = config()
-db_manager = DBManager('Head_Hunter', params)
+db_manager = DBManager('hh', params)
 db_manager.connect_db()
 db_manager.create_table()
 db_manager.write_info_in_table()
